8d43fbeb9f23d594c1995e7580eaa99f393b88ac
송기라 <thdrlfk93@naver.com>
2022-09-30 19:35:59 +0900

    오르막길 코드 수정

--- 0930_실습문제/im/오르막길.py
+++ 0930_실습문제/im/오르막길.py
@@ -5,26 +5,36 @@ N = int(input())
 high_road = list(map(int, input().split()))
 
 maxV = 0
-for i in range(N):
-    if 0 <= i+1 < N and 0 <= i -1 < N:
-        if high_road[i] < high_road[i+1]:
-            e = high_road[i + 1]
-            if maxV < high_road[i+1] - high_road[i]:
-                maxV = high_road[i + 1] - high_road[i]
-        elif high_road[i - 1] > high_road[i]:
-            s = high_road[i - 1]
-        if high_road[i] > high_road[i - 1]:
-            s = high_road[i - 1]
-            if maxV < high_road[i] - high_road[i - 1]:
-                maxV = high_road[i] - high_road[i - 1]
-        elif high_road[i+1] < high_road[i]:
-            e = high_road[i + 1]
-road = e - s
+ans = []
 
-if maxV < road:
-    maxV = road
+# for i in range(N):
+#     if 0 <= i+1 < N and 0 <= i -1 < N:
+#         if high_road[i] < high_road[i+1]:
+#             e = high_road[i + 1]
+#             if maxV < high_road[i+1] - high_road[i]:
+#                 maxV = high_road[i + 1] - high_road[i]
+#         elif high_road[i - 1] > high_road[i]:
+#             s = high_road[i - 1]
+#         if high_road[i] > high_road[i - 1]:
+#             s = high_road[i - 1]
+#             if maxV < high_road[i] - high_road[i - 1]:
+#                 maxV = high_road[i] - high_road[i - 1]
+#         elif high_road[i+1] < high_road[i]:
+#             e = high_road[i + 1]
+# road = e - s
+# if maxV < road:
+#     maxV = road
 
-print(maxV)
+for i in range(N-1):
+    if high_road[i] < high_road[i+1]:
+        maxV += high_road[i+1] - high_road[i]
+    elif high_road[i] >= high_road[i+1]:
+        ans.append(maxV)
+        maxV = 0
+
+ans.append(maxV)
+
+print(max(ans))
 
 # for i in range(N-1):
 #     for j in range(i+1, N):
