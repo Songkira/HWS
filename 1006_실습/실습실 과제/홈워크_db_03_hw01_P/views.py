c60f1482817e29d4c6474acde58221a777e8b6a5
sejin <ghkdehdud96@naver.com>
2022-10-06 17:52:40 +0900

    1006 DB실습

--- 1006_실습/실습실 과제/홈워크_db_03_hw01_P/views.py
+++ 1006_실습/실습실 과제/홈워크_db_03_hw01_P/views.py
@@ -1,10 +1,15 @@
 # views.py
 def detail(request, __(a)__):
     article = get_object_or_404(Article, pk=article_pk)
-
+    comments = article.comment_set.all()
     comment_form = CommentForm()
     context_data = {
         'article': article,
         __(b)__,
+        'comments':comments,
     }
     return render(request, 'eithers/detail.html', __(c)__)
+
+a => article_pk
+b => 'comment_form':comment_form,
+c => context_data
