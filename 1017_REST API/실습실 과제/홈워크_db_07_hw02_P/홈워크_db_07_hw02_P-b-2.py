18268642ed8d73725a09fd94045cc64910d49d0e
송기라 <thdrlfk93@naver.com>
2022-10-17 22:56:55 +0900

    1017 REST API

---
+++ 1017_REST API/실습실 과제/홈워크_db_07_hw02_P/홈워크_db_07_hw02_P-b-2.py
@@ -0,0 +1,17 @@
+# b- serializers.py
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    __(b)__ = __(a)__(
+        __(c)__,
+        __(d)__,
+    )
+
+    class Meta:
+        model = Article
+        fields = ('id', 'title', 'content', 'created_at', 'updated_at', '__(b)__')
+
+(a) => serializers.PrimaryKeyRelatedField
+(b) => comment_set
+(c) => many=True
+(d) => read_only=True
